9161051d7e183aff9a97e1828c8f18137916dd18
Callum Macpherson <callum.macphers@hackney.gov.uk>
2021-09-02 09:47:48 +0100

    Implement gateway

--- backend/DynamoDbNotesApp/Gateway/NotesGateway.cs
+++ backend/DynamoDbNotesApp/Gateway/NotesGateway.cs
@@ -1,38 +1,81 @@
-﻿using DynamoDbNotesApp.Boundary.Response;
+﻿using Amazon.DynamoDBv2.DataModel;
+using DynamoDbNotesApp.Boundary.Response;
 using DynamoDbNotesApp.Domain;
+using DynamoDbNotesApp.Factories;
 using DynamoDbNotesApp.Gateway.Interfaces;
+using DynamoDbNotesApp.Infrastructure;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Threading;
 using System.Threading.Tasks;
 
 namespace DynamoDbNotesApp.Gateway
 {
     public class NotesGateway : INotesGateway
     {
-        public Task<Guid> CreateNote(Note note)
+        private readonly IDynamoDBContext _context;
+
+        public NotesGateway(IDynamoDBContext databaseContext)
         {
-            throw new NotImplementedException();
+            _context = databaseContext;
         }
 
-        public Task<bool> DeleteNote(Guid id)
+        public async Task<Guid> CreateNote(Note note)
         {
-            throw new NotImplementedException();
+            var noteDb = note.ToDatabase();
+
+            await _context.SaveAsync(noteDb);
+
+            return noteDb.Id;
         }
 
-        public Task<List<Note>> GetAllNotes()
+        public async Task<bool> DeleteNote(Guid id)
         {
-            throw new NotImplementedException();
+            var noteExists = await NoteExists(id);
+            if (noteExists == false) return false;
+
+            await _context.DeleteAsync<NotesDb>(id);
+
+            return true;
         }
 
-        public Task<Note> GetNoteById(Guid id)
+        public async Task<List<Note>> GetAllNotes()
         {
-            throw new NotImplementedException();
+            var conditions = new List<ScanCondition>();
+
+            var response = await _context.ScanAsync<NotesDb>(conditions).GetRemainingAsync();
+
+            return response.Select(x => x.ToDomain()).ToList();
+
         }
 
-        public Task<bool> UpdateNote(Note note)
+        public async Task<Note> GetNoteById(Guid id)
         {
-            throw new NotImplementedException();
+            var response = await _context.LoadAsync<NotesDb>(id).ConfigureAwait(false);
+
+            return response?.ToDomain();
+        }
+
+        public async Task<bool> UpdateNote(Note note)
+        {
+            var existingNote = await GetNoteById(note.Id);
+            if (existingNote == null) return false; // note doesnt exist
+
+            var newNote = note.ToDatabase();
+            newNote.Created = (DateTime)existingNote.Created;
+            newNote.Modified = DateTime.UtcNow;
+
+            await _context.SaveAsync(newNote);
+
+            return true;
+        }
+
+        private async Task<bool> NoteExists(Guid id)
+        {
+            var response = await _context.LoadAsync<NotesDb>(id).ConfigureAwait(false);
+
+            return response != null;
         }
     }
 }
