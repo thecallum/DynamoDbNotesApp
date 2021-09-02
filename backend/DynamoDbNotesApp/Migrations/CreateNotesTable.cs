1480697bba672229259f6c5557771caa2f7fde2c
Callum Macpherson <callum.macphers@hackney.gov.uk>
2021-09-02 09:48:17 +0100

    Add migration file

---
+++ backend/DynamoDbNotesApp/Migrations/CreateNotesTable.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DynamoDbNotesApp.Migrations
+{
+    public class CreateNotesTable
+    {
+        private async Task CreateNotesTableAsync()
+        {
+
+            var clientConfig = new AmazonDynamoDBConfig { ServiceURL = "http://localhost:8000" };
+            var client = new AmazonDynamoDBClient(clientConfig);
+
+            string tableName = "Notes";
+
+            var request = new CreateTableRequest
+            {
+                TableName = tableName,
+                AttributeDefinitions = new List<AttributeDefinition>()
+                    {
+                        new AttributeDefinition
+                        {
+                            AttributeName = "id",
+                            AttributeType = "S"
+                        }
+                    },
+                KeySchema = new List<KeySchemaElement>()
+                  {
+                    new KeySchemaElement
+                    {
+                      AttributeName = "id",
+                      KeyType = "HASH"  //Partition key
+                    }
+                  },
+                ProvisionedThroughput = new ProvisionedThroughput
+                {
+                    ReadCapacityUnits = 2,
+                    WriteCapacityUnits = 2
+                }
+            };
+
+            await client.CreateTableAsync(request).ConfigureAwait(false);
+
+        }
+    }
+}
